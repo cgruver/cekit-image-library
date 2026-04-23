aa9329de218c8e1b54d54aa486dd6140296bc059
cgruver <cgruver@redhat.com>
2026-04-23 12:03:00 +0000

    comment out clg.lab specific configuration
    Signed-off-by: cgruver <cgruver@redhat.com>

--- modules/shared-artifacts/nodejs-install.sh
+++ modules/shared-artifacts/nodejs-install.sh
@@ -5,6 +5,9 @@ curl -fsSL -o ${TEMP_DIR}/node.tz https://nodejs.org/dist/${NODE_VERSION}/node-$
 tar -x --no-auto-compress -f ${TEMP_DIR}/node.tz -C ${TEMP_DIR}
 mv ${TEMP_DIR}/node-${NODE_VERSION}-linux-x64 /usr/local/node
 rm -rf "${TEMP_DIR}"
-mkdir /usr/local/node/etc
-mv /npmrc /usr/local/node/etc/npmrc
-chmod 444 /usr/local/node/etc/npmrc
+#
+# Example for enabling global npm settings
+#
+# mkdir -p /usr/local/node/etc
+# mv /npmrc /usr/local/node/etc/npmrc
+# chmod 444 /usr/local/node/etc/npmrc
